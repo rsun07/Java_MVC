ae1f9a60851796c2b6a4f0648f3f2402e7d0c44d
Xiaoming Sun <rsun2014@gmail.com>
2018-07-25 20:37:09 -0700

    SimpleUrlHandlerMapping

--- springmvc/xml-config/src/main/java/pers/xiaoming/javaweb/springmvc/MyController.java
+++ springmvc/xml-config/src/main/java/pers/xiaoming/javaweb/springmvc/MyController.java
@@ -1,17 +1,22 @@
 package pers.xiaoming.javaweb.springmvc;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@Slf4j
 public class MyController implements Controller {
     public ModelAndView handleRequest(HttpServletRequest httpServletRequest,
                                       HttpServletResponse httpServletResponse) throws Exception {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.addObject("msg", "Hello Spring MVC world");
-        modelAndView.setViewName("hello-world-springmvc.html");
+        modelAndView.setViewName("springmvc-config.html");
+
+        log.info("request is {}", httpServletRequest.toString());
+
         return null;
     }
 }
