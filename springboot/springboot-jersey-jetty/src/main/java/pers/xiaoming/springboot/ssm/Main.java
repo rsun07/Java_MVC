fcd30fd80d06df382f72271a7254e09200171907
Xiaoming Sun <rsun2014@gmail.com>
2018-08-01 16:03:42 -0700

    Initial springboot jersey jetty. Replace Tomcat to Jetty

---
+++ springboot/springboot-jersey-jetty/src/main/java/pers/xiaoming/springboot/ssm/Main.java
@@ -0,0 +1,9 @@
+package pers.xiaoming.springboot.ssm;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAppConfig.class, args);
+    }
+}
---
+++ springboot/springboot-jersey-jetty/src/main/java/pers/xiaoming/springboot/ssm/entity/Student.java
@@ -0,0 +1,30 @@
+package pers.xiaoming.springboot.ssm.entity;
+
+public class Student {
+    private int id;
+    private String name;
+    private double score;
+
+    public Student() {
+    }
+
+    public Student(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public Student(int id, String name, double score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+}
